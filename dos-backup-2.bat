23111f75dd27d8a8b4b57ac829c56df83c485455
vpanagos <vpanagos@yahoo.com>
2020-04-21 13:40:21 +0300

    Create dos-backup-2.bat

---
+++ dos-backup-2.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set zip=%programfiles%\7-zip\7z.exe
+set outputpath="C:\users\pvoutskidis\Desktop\"
+set outputfile=BACKUP-SERVER-
+set zipfile="%outputfile%%DATE:~-4%-%DATE:~3,2%-%DATE:~7,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~-5,2%.7z"
+
+set output=%outputpath%%outputfile%
+
+set dir1="C:\users\pvoutskidis\Desktop\VPN-NEW"
+set dir2="%programfiles%\\OpenVPN"
+
+cd "%outputpath%"
+
+forfiles -s -m %outputfile%*.* /d -5 -c "cmd /c del @path"
+
+"%zip%" a -t7z %zipfile% %dir1% %dir2%%
