f0119c01d92da1348add8873dfc79f33066262e8
vpanagos <vpanagos@yahoo.com>
2020-05-01 13:12:41 +0300

    Update dos-backup-2.bat

--- dos-backup-2.bat
+++ dos-backup-2.bat
@@ -1,13 +1,13 @@
 @echo off
 
 set zip=%programfiles%\7-zip\7z.exe
-set outputpath="C:\users\pvoutskidis\Desktop\"
+set outputpath="C:\users\user\Desktop\"
 set outputfile=BACKUP-SERVER-
 set zipfile="%outputfile%%DATE:~-4%-%DATE:~3,2%-%DATE:~7,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~-5,2%.7z"
 
 set output=%outputpath%%outputfile%
 
-set dir1="C:\users\pvoutskidis\Desktop\VPN-NEW"
+set dir1="C:\users\user\Desktop\VPN-NEW"
 set dir2="%programfiles%\\OpenVPN"
 
 cd "%outputpath%"
